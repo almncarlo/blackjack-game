6d109d9c396fbd1e84746389e1d94c30bdddff59
Carlo Alamani <carlo.alamani@gmail.com>
2022-12-24 10:00:46 +0800

    added printing of player and game instances

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def blackjackmenu2():
 
 
 class Player:
-    def __init__(self, name, money, strategy):
+    def __init__(self, name='default', money=89000):
         self.name = name
         self.money = int(money)
+    
+    def __str__(self):
+        return f'{self.name} {self.money}'
 
 
 class Game:
-    def __init__(self, name, num_decks, standardbet):
+    def __init__(self, name='defaultgame', num_decks=8, standardbet=10):
         self.suits = ['C','D','H','S']
         self.values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
         self.cardlist = []
@@ -24,8 +27,14 @@ class Game:
                 for j in self.values:
                     self.cardlist.append((j,i))
 
+        self.name = name
+        self.standardbet = standardbet
+
     def rungame(self, player, automated = False):
         pass
+
+    def __str__(self):
+        return f'{self.name} {self.standardbet}'
 
 
 if __name__ == '__main__':
@@ -39,24 +48,28 @@ if __name__ == '__main__':
         if n == 1:
             name, money = input().split(',')
             money = int(money)
-
-            # process new player
             player_list.append(Player(name, money))
 
         # Choose a player
         elif n == 2:
-            pass
+            list_idx = 1
+            for p in sorted(player_list, key = lambda x:x.name):
+                 print(list_idx, p)
+                 list_idx += 1
+
         # Initialize a game
         elif n == 3:
             name, num_decks, standardbet = input().split(',')
             num_decks, standardbet = int(num_decks), int(standardbet)
-            
-            # process new game
             game_list.append(Game(name, num_decks, standardbet))
-            
+
         # Choose a game
         elif n == 4:
-            pass
+            list_idx = 1
+            for g in sorted(game_list, key = lambda x:x.name):
+                 print(list_idx, g)
+                 list_idx += 1
+
         # Play a game
         elif n == 5:
             pass
